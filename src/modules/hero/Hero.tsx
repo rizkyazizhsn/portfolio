c9776cbbc887bca45ca2ea1fda2a74befeab01fa
rizkyaziz <rizkyaziz.hsn@gmail.com>
2025-08-20 13:35:07 +0700

    ref: add image hero

--- src/modules/hero/Hero.tsx
+++ src/modules/hero/Hero.tsx
@@ -1,21 +1,22 @@
 import { motion } from "motion/react";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <section className="container relative py-24">
+    <section className="container relative py-14 md:py-24">
       <motion.div
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ ease: "easeOut", duration: 0.4, delay: 0.6 }}
-        className="absolute block rounded-full bg-primary/80 size-56 sm:size sm:size-80 blur-[100px] sm:blur-[200px] -top-10 -left-10 sm:-top-28 sm:-left-28"
+        className="absolute block rounded-full bg-primary/90 size-56 sm:size-80 blur-[100px] sm:blur-[200px] -top-10 -left-10 sm:-top-28 sm:-left-28"
       />
       <motion.div
         initial={{ opacity: 0, scale: 0.8 }}
         animate={{ opacity: 1, scale: 1 }}
         transition={{ ease: "easeOut", duration: 0.4, delay: 0.7 }}
-        className="absolute block rounded-full bg-primary/80 size-56 sm:size-80 blur-[100px] sm:blur-[200px] bottom-0 -right-10 sm:top-36 sm:-right-14"
+        className="absolute block rounded-full bg-primary size-56 sm:size-80 blur-[100px] sm:blur-[150px] bottom-0 -right-10 sm:bottom-52 sm:-right-10"
       />
-      <div className="flex flex-col sm:flex-row items-center justify-between md:gap-x-20">
+      <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-between md:gap-x-20">
         {/* Left Content */}
         <div className="flex flex-col gap-y-6">
           {/* Badge */}
@@ -65,9 +66,9 @@ const Hero = () => {
               initial={{ y: "50%", opacity: 0 }}
               animate={{ y: 0, opacity: 1 }}
               transition={{ duration: 0.5, delay: 0.5 }}
-              className="text-base md:text-lg sm:w-3/4"
+              className="text-base md:text-lg"
             >
-              Frontend Developer With 5+ years of frontend expertise, I craft clean, performant,
+              Frontend Developer With 5+ years of frontend expertise, <br /> I craft clean, performant,
               and delightful experiences powered by modern technologies.
             </motion.p>
           </div>
@@ -77,7 +78,7 @@ const Hero = () => {
               initial={{ opacity: 0, y: "100%" }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ ease: "easeOut", duration: 0.4, delay: 0.4 }}
-              className="bg-primary w-full sm:w-auto text-sm md:text-base px-7 sm:px-8 md:px-12 py-3 rounded-full cursor-pointer"
+              className="bg-primary w-full sm:w-auto text-sm px-7 sm:px-8 md:px-12 py-3 rounded-full cursor-pointer"
             >
               Let’s talk
             </motion.button>
@@ -85,7 +86,7 @@ const Hero = () => {
               initial={{ opacity: 0, y: "100%" }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.4, delay: 0.7 }}
-              className="border border-primary/80 w-full sm:w-auto text-sm md:text-base px-7 sm:px-8 md:px-12 py-3 rounded-full cursor-pointer"
+              className="border border-primary/80 w-full sm:w-auto text-sm px-7 sm:px-8 md:px-12 py-3 rounded-full cursor-pointer"
             >
               Download CV
             </motion.button>
@@ -97,8 +98,12 @@ const Hero = () => {
             initial={{ opacity: 0, scale: 0.95 }}
             animate={{ opacity: 1, scale: 1 }}
             transition={{ duration: 0.5, delay: 0.5 }}
-            className="h-80 w-56 md:h-[550px] md:w-[400px] bg-primary rounded-lg"
-          ></motion.div>
+            className="relative h-80 w-56 md:h-[550px] md:w-[400px] bg-primary rounded-lg overflow-hidden"
+          >
+            <motion.div initial={{ y: "5%", opacity: 0 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 0.5, delay: 0.6 }} className="size-full">
+              <Image src="/img/portfolio-3.1.png" alt="hero" width={500} height={550} className="size-full object-contain absolute bottom-0" />
+            </motion.div>
+          </motion.div>
         </div>
       </div>
     </section>
